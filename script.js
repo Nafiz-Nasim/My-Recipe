11e3700f141a50a093c7dfb8885eeed665d10081
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-07-03 16:56:15 +0600

    add random recipes api

---
+++ script.js
@@ -0,0 +1,72 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const randomRecipeContainer = document.getElementById('randomRecipe');
+
+// Function to fetch random recipes
+const randomrecipes = async () => {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        const data = await response.json();
+
+        const recipe = data.meals[0];
+        displayRecipe(recipe); 
+    } catch (error) {
+        console.error("Error fetching recipe:", error);
+    }
+};
+
+
+const displayRecipe = (recipe) => {
+  
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = recipe[`strIngredient${i}`];
+        const measure = recipe[`strMeasure${i}`];
+        if (ingredient) {
+            ingredients.push(`${ingredient} - ${measure}`);
+        }
+    }
+
+    const recipeHTML = `
+
+    <div class="sm:mx-20 "> <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" class="w-full rounded-md mb-4 sm:m-10"></div>
+   
+    <div class=" ">
+    <div class="">
+        <h2 class="text-xl font-semibold mb-2">${recipe.strMeal}</h2>
+    <p class="text-gray-600 mb-4">Category: ${recipe.strCategory}</p>
+    
+    <!-- Ingredients Section -->
+    <div class="ingredients mb-4 ">
+      <h3 class="font-semibold text-lg">Ingredients</h3>
+      <ul class="list-disc pl-5 text-gray-700">
+        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+      </ul>
+    </div>
+
+    <!-- Instructions Section -->
+    <div class="instructions mb-4">
+      <h3 class="font-semibold text-lg">Instructions</h3>
+      <p class="text-gray-700">${recipe.strInstructions.slice(0, 200)}...</p>
+    </div>
+    <!-- See Full Details Link -->
+    <a href="${recipe.strSource}" target="_blank" class="text-blue-500 hover:underline mt-4 inline-block">See Full Details</a>
+    
+    
+    </div>
+ </div>
+   
+
+    
+  `;
+    randomRecipeContainer.innerHTML = recipeHTML;
+};
+
+
+window.onload = randomrecipes;
+
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+});
